e9162eaa857a34922e1b3623f6ccdca4f4109638
清风 <qingfeng@shouhouyi.net>
2020-09-27 12:35:02 +0800

    多项目模板

---
+++ multi-module-api/src/main/java/com/example/multimoduleapi/MultiModuleApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.multimoduleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiModuleApiApplication.class, args);
+    }
+}
---
+++ multi-module-api/src/test/java/com/example/multimoduleapi/MultiModuleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multimoduleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
